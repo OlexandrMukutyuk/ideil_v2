1ed6c2b265bc633604f9500662c5ee884efabcac
SunSet <sasawa200@gmail.com>
2023-12-24 22:08:16 +0200

    createt migrations

---
+++ project/CityCard/database/migrations/2023_12_24_193002_create_user__city_cards_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user__city_cards', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("name");
+            $table->string("phone_number");
+            $table->string("city");
+            $table->decimal("purse");
+            $table->string("login");
+            $table->string("password");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user__city_cards');
+    }
+};

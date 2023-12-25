3ff84ba2339e4691f3b8eabfae203e8c21c52a90
SunSet <sasawa200@gmail.com>
2023-12-25 14:19:47 +0200

    adeed singin view

--- project/CityCard/app/Http/Middleware/CheckRole.php
+++ project/CityCard/app/Http/Middleware/CheckRole.php
@@ -15,13 +15,14 @@ class CheckRole
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // Перевірка, чи у користувача є роль 'admin'
+
         if ($request->user()->hasRole('admin')) {
-            // Якщо так, продовжуємо виконання запиту
+
             return $next($request);
         }
 
-        // Якщо користувач не має ролі 'admin', можна відправити відповідь з помилкою або перенаправити куди-небудь
+
+        
         return response('Access denied. You do not have the required role.', 403);
     }
 }
